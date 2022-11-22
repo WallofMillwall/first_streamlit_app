68539c1121dc8025440f5cc3f101908486da655a
WallOfMillwall <118762509+WallofMillwall@users.noreply.github.com>
2022-11-22 10:36:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,4 @@ sl.text('🥑🍞Avocado Toast')
 
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+sl.dataframe(my_fruit_list)
